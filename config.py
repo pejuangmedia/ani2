91395f69d21daa28a86471f2e0d50ea7e703e2c0
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-10-13 08:01:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from telethon import TelegramClient
 
 api_id = '7120601' #os.environ.get('API_ID')
 api_hash = 'aebd45c2c14b36c2c91dec3cf5e8ee9a'  #os.environ.get('API_HASH')
-bot_token = '1920905087:AAE-3G2wZsjpuuSnWzQT8z_jbzwvQFAQ154' #os.environ.get('BOT_TOKEN')
+bot_token = '1920905087:AAG_xCvsdjxVu8VUDt9s4JhD22ND-UIJttQ' #os.environ.get('BOT_TOKEN')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
